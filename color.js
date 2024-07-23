422099c3bcea5962f563778cc79fad81721c11a8
Aarthiragu <117432166+Aarthiragu@users.noreply.github.com>
2024-07-23 16:40:21 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,12 @@
+const colors = ["red", "black", "orange", "yellow", "purple", "blue"];
+let currentColorIndex = 0;
+
+document.getElementById('color-button').addEventListener('click', function() {
+   
+    currentColorIndex = (currentColorIndex + 1) % colors.length;
+
+    const newColor = colors[currentColorIndex];
+    document.body.style.backgroundColor = newColor;
+    document.getElementById('color-name').textContent = newColor.charAt(0).toUpperCase() + newColor.slice(1);
+});
+
